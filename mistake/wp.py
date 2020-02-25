031a500fb80169480021f558805ce00fc7ecfcc8
bootcap <daniel.zhang.2022@gmail.com>
2020-02-25 23:35:21 +0800

    update

--- mistake/wp.py
+++ mistake/wp.py
@@ -1,7 +1,7 @@
 from pwn import *
 
 context(log_level = 'debug', os = 'linux', arch = 'amd64')
-r = process('./mistake')
+r = process('/home/mistake/mistake')
 
 print r.recvuntil('do not bruteforce...\n')
 r.send('1111111111\n0000000000\n')
